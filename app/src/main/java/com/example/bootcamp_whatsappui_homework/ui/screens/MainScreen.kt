1b8d1e74b058bb1e65e37a528800d528d774201d
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-12-24 16:21:00 +0300

    Some words added into strings.xml

--- app/src/main/java/com/example/bootcamp_whatsappui_homework/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/bootcamp_whatsappui_homework/ui/screens/MainScreen.kt
@@ -1,11 +1,8 @@
 package com.example.bootcamp_whatsappui_homework.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.BadgedBox
