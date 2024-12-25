81516071091d38abff969e96ff894c7079429243
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-12-25 15:20:22 +0300

    BottomBar Item titles taken and arranged from strings.xml

--- app/src/main/java/com/example/bootcamp_whatsappui_homework/model/repository/GetBottomTabBarItems.kt
+++ app/src/main/java/com/example/bootcamp_whatsappui_homework/model/repository/GetBottomTabBarItems.kt
@@ -2,6 +2,7 @@ package com.example.bootcamp_whatsappui_homework.model.repository
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.bootcamp_whatsappui_homework.R
 import com.example.bootcamp_whatsappui_homework.model.entity.BottomTabBarItems
 
@@ -9,22 +10,22 @@ import com.example.bootcamp_whatsappui_homework.model.entity.BottomTabBarItems
 fun GetBottomTabBarItems(): List<BottomTabBarItems> {
     return listOf(
         BottomTabBarItems(
-            title = "Chats",
+            title = stringResource(id = R.string.bottomBar_1),
             selectedIcon = painterResource(id = R.drawable.chat_selected),
             unselectedIcon = painterResource(id = R.drawable.chat_unselected)
         ),
         BottomTabBarItems(
-            title = "Updates",
+            title = stringResource(id = R.string.bottomBar_2),
             selectedIcon = painterResource(id = R.drawable.status),
             unselectedIcon = painterResource(id = R.drawable.status)
         ),
         BottomTabBarItems(
-            title = "Communities",
+            title = stringResource(id = R.string.bottomBar_3),
             selectedIcon = painterResource(id = R.drawable.groups_selected),
             unselectedIcon = painterResource(id = R.drawable.groups_unselected)
         ),
         BottomTabBarItems(
-            title = "Calls",
+            title = stringResource(id = R.string.bottomBar_4),
             selectedIcon = painterResource(id = R.drawable.call_selected),
             unselectedIcon = painterResource(id = R.drawable.call_unselected)
         )
